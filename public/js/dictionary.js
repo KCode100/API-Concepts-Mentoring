c02c4082afee1dbd26f8f1effb1d43a9c54d77a2
Kivi Corn <akiva.corn@cyber-duck.co.uk>
2021-10-07 15:27:51 +0100

    complete dictionary API

--- public/js/dictionary.js
+++ public/js/dictionary.js
@@ -2,21 +2,50 @@
 const btnEl = document.querySelector('.dict-btn');
 const wordInputEl = document.querySelector('.word-input');
 const soundEl = document.querySelector('.speak');
+const wordEl = document.querySelector('.word');
+const defEl = document.querySelector('.definition');
+const exampleEl = document.querySelector('.example');
+const synonymsEl = document.querySelector('.synonyms');
+
+// GLOBALS
+let word = 'inevitable';
+
+function display(word, definition, example, synonyms) {
+	// Word
+	wordEl.textContent = word;
+	// definition
+	defEl.textContent = definition;
+	// example
+	exampleEl.textContent = `"${example}"`;
+	// synonyms
+	if (synonyms.length === 0) {
+		document.querySelector('.syn-section').hidden = true;
+	} else {
+		document.querySelector('.syn-section').hidden = false;
+		synonymsEl.innerHTML = '';
+		synonyms.forEach((el) => {
+			const synonym = document.createElement('li');
+			synonym.textContent = el;
+			synonymsEl.appendChild(synonym);
+		});
+	}
+}
 
 function displayWordResults(data) {
 	const definition = data[0].meanings[0].definitions[0].definition;
+	const synonyms = data[0].meanings[0].definitions[0].synonyms;
 	const example = data[0].meanings[0].definitions[0].example;
 	const sound = data[0].phonetics[0].audio;
 	console.log(data);
-	console.log(definition);
-	console.log(example);
-	console.log(sound);
-	document.querySelector('audio').setAttribute('src', sound);
+	display(word, definition, example, synonyms);
+
+	// Set audio
+	document.querySelector('audio').src = sound;
 }
 
 function getWordData(e) {
 	e.preventDefault();
-	const word = wordInputEl.value;
+	word = wordInputEl.value;
 	const dictionaryUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 	fetch(dictionaryUrl)
 		.then((res) => res.json())
@@ -29,3 +58,4 @@ function playAudio() {
 
 btnEl.addEventListener('click', getWordData);
 soundEl.addEventListener('click', playAudio);
+getWordData();
